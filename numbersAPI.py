efa60ff66f2bca9e289866468fc796dfced311b6
Alina <px-mew@mail.ru>
2022-06-29 18:17:37 +0300

    Python основы и применения

---
+++ numbersAPI.py
@@ -0,0 +1,31 @@
+"""
+В этой задаче вам необходимо воспользоваться API сайта numbersapi.com
+
+Вам дается набор чисел. Для каждого из чисел необходимо узнать, существует ли интересный математический факт об этом числе.
+
+Для каждого числа выведите Interesting, если для числа существует интересный факт, и Boring иначе.
+Выводите информацию об интересности чисел в таком же порядке, в каком следуют числа во входном файле.
+
+Пример запроса к интересному числу:
+http://numbersapi.com/31/math?json=true
+
+Пример запроса к скучному числу:
+http://numbersapi.com/999/math?json=true
+"""
+
+import requests
+import json
+
+f = open("dataset_24476_3.txt", "r")
+for line in f:
+    line = line.rstrip()
+    num = int(line)
+    template = 'http://numbersapi.com/{}/math?json=true'
+    res = requests.get(template.format(num))
+    data_json = res.text
+    data_again = json.loads(data_json)
+    if data_again["found"]:
+        print("Interesting")
+    else:
+        print("Boring")
+f.close()
